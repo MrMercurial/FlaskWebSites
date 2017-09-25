9e96962ef9b927cc0b89980d40cff8b5e0315974
MrMercurial <taoanqi001@163.com>
2017-09-25 20:50:38 +0800

    20170925
    第二次提交，学习了flask-bootstrap

--- FlaskWebSites.py
+++ FlaskWebSites.py
@@ -1,7 +1,9 @@
 import sqlite3
-from flask import Flask,request,g,session,redirect,url_for
+from flask import Flask,request,g,session,redirect,url_for,render_template
+from flask.ext.bootstrap import Bootstrap
 
 app=Flask(__name__)
+bootstrap=Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -10,7 +12,7 @@ def index():
 
 @app.route('/user/<username>')
 def Hello_User(username):
-    return 'Hello %s'%username
+    return render_template('user.html',name=username)
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run()
---
+++ static/JS/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ant on 2017/9/25.
+ */
